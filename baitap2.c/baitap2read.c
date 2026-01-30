d40340bf46b0169872e5500dd00142b88d4c4d71
Lê Chí Vĩ <capot2007@gmail.com>
2026-01-30 15:02:54 +0700

    Add files via upload

---
+++ baitap2.c/baitap2read.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+
+
+struct SACH{
+	char masach[20];
+	char tieude[100];
+	char tacgia[50];
+	int namxuatban;
+	long long giaban;
+	int sotrang;
+	char nhaxuatban[100];
+	char loaisach[50];
+};
+typedef struct SACH S;
+
+void nhapsach(S *a);
+void xuatsach(S a);
+void nhapsosach(S a[], int n);
+void xuatsosach(S a[], int n);
+
+int main(){
+	S a[100];
+	int n=2;
+	
+	
+	FILE *fp;
+	fp=fopen("sosach.out", "rb");
+	if(fp!=NULL){
+		fread(a, sizeof(S), n, fp);
+		fclose(fp);
+	}else{printf("loi");}
+	
+	xuatsosach(a, n);
+	return 0;
+}
+
+void nhapsach(S *a)
+{
+	printf("\nnhap ma sach: ");
+	fflush(stdin);
+	scanf("%s", a->masach);
+	printf("\nnhap tieu de: ");
+	fflush(stdin);
+	gets(a->tieude);
+	printf("\nnhap tac gia: ");
+	fflush(stdin);
+	gets(a->tacgia);
+		printf("\nnhap nam xuat ban: ");
+	fflush(stdin);
+	scanf("%d", &a->namxuatban);
+		printf("\nnhap gia ban: ");
+	fflush(stdin);
+	scanf("%lld", &a->giaban);
+		printf("\nnhap so trang: ");
+	fflush(stdin);
+	scanf("%d", &a->sotrang);
+		printf("\nnhap nha xuat ban: ");
+	fflush(stdin);
+	gets(a->nhaxuatban);
+		printf("\nnhap loai sach: ");
+	fflush(stdin);
+	gets(a->loaisach);
+}
+
+void xuatsach(S a)
+{
+	printf("\n%s\t%s\t%s\t%d\t%lld\t%d\t%s\t%s", a.masach, a.tieude, a.tacgia, a.namxuatban, a.giaban, a.sotrang, a.nhaxuatban, a.loaisach);
+}
+
+void nhapsosach(S a[], int n)
+{
+	int i=0;
+	while(i<n){
+		printf("\nnhap sach tai vi tri %d", i);
+		nhapsach(&a[i]);
+		i++;
+	}
+}
+
+void xuatsosach(S a[], int n){
+	int i=0;
+	while(i<n){
+		printf("\nsach tai vi tri %d", i);
+		xuatsach(a[i]);
+		i++;
+	}
+}
